5c954fc6d8506a4043dd1b934f61f949e465067f
MitchReed123 <reed.mitch@live.com>
2016-07-19 13:47:40 -0400

    First push from the WIP

---
+++ Redo/Models/GameModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Redo.Models
+{
+    public class GameModel
+    {
+        [Display(Name = "Enter Your Name")]
+        [Required(ErrorMessage = "You must enter your name")]
+        [MinLength(1, ErrorMessage = " Name is too short")]
+        [MaxLength(20, ErrorMessage = "Name is too long")]
+        public string PlayerName { get; set; }
+
+        [Display(Name = "What is your guess?")]
+        [Required(ErrorMessage = "Guess is Required")]
+        [Range(1, 10, ErrorMessage = "Guess Must be Between 1 and 20")]
+        public int Guess { get; set; }
+
+        public override string ToString()
+        {
+            return $"{PlayerName} ({Guess})";
+        }
+    }
+}
